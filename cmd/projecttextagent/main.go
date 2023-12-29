bbd779c343b260d6a0beb845c5c5c77bcc078eb7
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-30 00:19:05 +0100

    added db logic template to main package

--- cmd/projecttextagent/main.go
+++ cmd/projecttextagent/main.go
@@ -9,6 +9,20 @@ func main() {
 	tempPath := "path/to/temp"
 	projectPath := "path/to/project"
 	ignorePatterns := "pattern1,pattern2"
+
+	// Initialize PostgreSQL connection
+	pgDB, err := postgres.New("your_postgres_connection_string")
+	if err != nil {
+		log.Fatalf("Failed to connect to PostgreSQL: %v", err)
+	}
+	defer pgDB.Close()
+
+	// Initialize VectorDB connection
+	vectorDB, err := vectordb.New(yourVectorDBConfig)
+	if err != nil {
+		log.Fatalf("Failed to connect to VectorDB: %v", err)
+	}
+
 	fileChanged := make(chan string)
 	go watchDirectory(path, fileChanged)
 
