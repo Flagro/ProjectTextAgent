72dffd3d39412b6375c0c951513ac4e5271f2dd8
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-09 19:39:36 +0100

    implemented json unmarshall into golang struct

--- pkg/fileparser/fileparser.go
+++ pkg/fileparser/fileparser.go
@@ -1,12 +1,12 @@
 package fileparser
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"os/exec"
 )
 
-type TextTableScoopOutput struct {
+type TextTableScoopOutput []struct {
 	FilePath string `json:"file_path"`
 	Data     []struct {
 		DataType string      `json:"data_type"`
@@ -15,16 +15,22 @@ type TextTableScoopOutput struct {
 	} `json:"data"`
 }
 
-func ParseFile(filePath, tempPath, projectPath, ignorePatterns string) string {
+func ParseFile(filePath, tempPath, projectPath, ignorePatterns string) TextTableScoopOutput {
 	// Construct the command
 	cmd := exec.Command("texttablescoop", filePath, "--temp", tempPath, "--project", projectPath, "--ignore", ignorePatterns)
 
 	// Run the command
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("Command finished with error: %v", err)
+		log.Fatalf("texttablescoop finished with error: %v", err)
 	}
 
-	fmt.Println(string(output))
-	return string(output)
+	// Unmarshal the output into a TextTableScoopOutput
+	var result TextTableScoopOutput
+	err = json.Unmarshal(output, &result)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal output: %v", err)
+	}
+
+	return result
 }
